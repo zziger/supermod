3750133f2d6d88141bfdf8fb205ee96a91fe0f39
Artem Dzhemesiuk <zziger07@gmail.com>
2022-12-27 02:42:53 +0100

    Added game::Asset and game::AssetPool classes

---
+++ src/game/AssetPool.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <string>
+
+#include "Asset.h"
+
+namespace game
+{
+    class AssetPool {
+    public:
+        virtual ~AssetPool() = default;
+        Asset* assets[1024];
+        int assetCount;
+
+        Asset* GetByName(const std::string& name) const {
+            for (auto i = 0; i < assetCount; i++) {
+                if (assets[i]->name == name) return assets[i];
+            }
+            
+            return nullptr;
+        }
+
+        static AssetPool* GetInstance() {
+            return (AssetPool*) 0x1423CF8;
+        }
+    };
+}
