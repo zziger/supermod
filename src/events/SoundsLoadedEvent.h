4f8b192d6f0bb2eeecb4778f3ac2970426a3ebdc
Artem Dzhemesiuk <zziger07@gmail.com>
2023-01-14 20:05:49 +0100

    Fixed sounds loading pattern on older game versions

--- src/events/SoundsLoadedEvent.h
+++ src/events/SoundsLoadedEvent.h
@@ -10,12 +10,15 @@
 struct SoundsLoadedEvent final : IEvent<"soundsLoaded"> {
 };
 
-HOOK_FN(int, load_sounds, ARGS()) {
+HOOK_FN(inline int, load_sounds, ARGS()) {
     const auto res = load_sounds_orig();
+    Log::Debug << "Звуки игры загружены" << Log::Endl;
     EventManager::Emit(SoundsLoadedEvent{});
     return res;
 }
 
 inline EventManager::Ready $sounds_loaded_event_hook([] {
-    HookManager::RegisterHook("55 8B EC 81 EC ? ? ? ? A1 ? ? ? ? 89 45 ? 68 ? ? ? ? E8", HOOK_REF(load_sounds));
+    static constexpr Memory::Pattern pat("E8 ? ? ? ? E8 ? ? ? ? B9 ? ? ? ? E8 ? ? ? ? B9 ? ? ? ? E8 ? ? ? ? B9");
+    static auto mem = pat.Search().GoToNearCall();
+    mem.Detour(HOOK_REF(load_sounds));
 });
