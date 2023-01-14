3c11257d860357a8ab5e6fef2b588b64e7307923
Artem Dzhemesiuk <zziger07@gmail.com>
2023-01-14 16:05:28 +0100

    Fixed build script and CI

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
 cmake -BBUILD . -A Win32 -DVERSION="%1"
-cmake --build BUILD --config Release
-IF EXIST BUILD\Release\dinput8.dll DEL /f BUILD\Release\dinput8.dll
-copy BUILD\Release\supercow-mod.dll BUILD\Release\dinput8.dll
+cmake --build BUILD --config RelWithDebInfo
