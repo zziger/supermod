da715425b101b4686f68fcec936ec24921532bf7
Artem Dzhemesiuk <programming@zziger.me>
2024-03-28 01:01:31 +0100

    fix: early tick event

--- src/events/TickEvent.h
+++ src/events/TickEvent.h
@@ -6,8 +6,9 @@
 
 struct BeforeTickEvent final : IEvent<"beforeTick", BeforeTickEvent> {};
 struct AfterTickEvent final : IEvent<"afterTick", AfterTickEvent> {};
+struct EarlyTickEvent final : IEvent<"earlyTick", EarlyTickEvent> {};
 
-typedef BeforeTickEvent TickEvent; 
+typedef BeforeTickEvent TickEvent;
 
 inline int (__thiscall *render_orig)(void* this_) = nullptr;
 inline int  __fastcall render(void* this_, void*) {
@@ -16,7 +17,13 @@ inline int  __fastcall render(void* this_, void*) {
     EventManager::Emit(AfterTickEvent());
     return res;
 }
+inline int (__thiscall *main_loop_orig)(void* this_) = nullptr;
+inline int  __fastcall main_loop(void* this_, void*) {
+    EventManager::Emit(EarlyTickEvent());
+    return main_loop_orig(this_);
+}
 
 inline EventManager::Ready $tick_event_hook([] {
     HookManager::RegisterHook("55 8B EC 51 E8 ? ? ? ? 6A ?", HOOK_REF_FORCE(render));
+    HookManager::RegisterHook("55 8B EC 83 3D ? ? ? ? ? 75 ? 68", HOOK_REF_FORCE(main_loop));
 });
